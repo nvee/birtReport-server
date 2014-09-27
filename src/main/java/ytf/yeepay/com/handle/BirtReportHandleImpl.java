be450c6694e458423d421097dcfaf59c62475c7b
nvee <1059587098@qq.com>
2014-09-27 12:46:15 +0800

    接口

---
+++ src/main/java/ytf/yeepay/com/handle/BirtReportHandleImpl.java
@@ -0,0 +1,13 @@
+package ytf.yeepay.com.handle;
+
+import java.util.Map;
+
+public class BirtReportHandleImpl implements BirtReportHandle{
+
+	@Override
+	public void createBirtReport(Map<String, Object> params) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
